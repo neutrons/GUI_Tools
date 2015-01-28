58c8217f7d0a7340e2e11441a922da13f4b32117
millersd <millersd@ornl.gov>
2015-01-28 17:42:25 -0500

    Added Table Widget Example
    Illustrates how to construct a table which users can move, add, and
    delete rows.

---
+++ Table-1/config.py
@@ -0,0 +1,16 @@
+#config.py
+#global definitions for constants and variables
+
+#Table column heading indicies
+firstName=1
+lastName=0
+favIceCream=2
+favColor=3
+select=4
+
+#define direction flags used to direct moving rows in the table
+up=0
+down=1
+
+#enable (True) or disable (False) built-in table column sorting
+colSort=False

7873b496e9f3284bc776c16c6ac7f623e8f7241f
JeanBilheux <bilheuxjm@ornl.gov>
2015-05-06 10:26:02 -0400

    Implemented the noThread method and the displayMessageInBox event as well

--- Thread/run_me.py
+++ Thread/run_me.py
@@ -1,8 +1,15 @@
 import sys
+import time
 from PyQt4 import QtGui, QtCore, Qt
 
 from main_window_interface import Ui_MainWindow
 
+#include this try/except block to remap QString needed when using IPython
+try:
+    _fromUtf8 = QtCore.QString.fromUtf8
+except AttributeError:
+    _fromUtf8 = lambda s: s
+    
 class ThreadApplication(QtGui.QMainWindow):
 
     #initialize app
@@ -11,22 +18,33 @@ class ThreadApplication(QtGui.QMainWindow):
         QtGui.QMainWindow.__init__(self, parent)
         self.ui = Ui_MainWindow() 
         self.ui.setupUi(self)
-    
-    def exit(self):
-        print 'in exit'
+        
+        QtCore.QObject.connect(self.ui.pushButton, QtCore.SIGNAL(_fromUtf8("clicked()")), self.noThread)
+        QtCore.QObject.connect(self.ui.pushButton_5, QtCore.SIGNAL(_fromUtf8("clicked()")), self.displayMessageInBox)        
 
-    def closeEvent(self, event=None):
-        # triggered when user exit application using top corner button (exit button)
-        reply = QtGui.QMessageBox.question(self, 'Message',
-                                           "Are you sure to quit?", QtGui.QMessageBox.Yes | 
-                                           QtGui.QMessageBox.No, QtGui.QMessageBox.No)
+    def displayMessageInBox(self):
+        self.ui.textEdit.append("Printing message here")
     
-        if reply == QtGui.QMessageBox.Yes:
-        #close application
-            event.accept()
-        else:
-        #do nothing and return
-            event.ignore()
+    def noThread(self):
+        self.ui.textEdit.append("No Thread")
+        QtGui.QApplication.processEvents()
+        for i in range(5):
+            time.sleep(2)
+            self.ui.textEdit.append("-> index loop %d" %i)
+            QtGui.QApplication.processEvents()
+    
+    #def closeEvent(self, event=None):
+        ## triggered when user exit application using top corner button (exit button)
+        #reply = QtGui.QMessageBox.question(self, 'Message',
+                                           #"Are you sure to quit?", QtGui.QMessageBox.Yes | 
+                                           #QtGui.QMessageBox.No, QtGui.QMessageBox.No)
+    
+        #if reply == QtGui.QMessageBox.Yes:
+        ##close application
+            #event.accept()
+        #else:
+        ##do nothing and return
+            #event.ignore()
 
 if __name__=="__main__":
     app = QtGui.QApplication(sys.argv)

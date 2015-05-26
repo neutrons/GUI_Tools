9928f385a0c705f22df51ffb8b824254db6b79bf
JeanBilheux <bilheuxjm@ornl.gov>
2015-05-26 14:32:36 -0400

    Just figured out how to display a message when running test.

--- UnitDocTests/unitTestFolder/multiplicator_test.py
+++ UnitDocTests/unitTestFolder/multiplicator_test.py
@@ -7,14 +7,17 @@ class TestMultiplicator(unittest.TestCase):
         pass
     
     def test_list_by_number(self):
+        '''This message will be displayed if this test fails'''
         multi1 = Multiplicator([3,4],2)
         self.assertEqual(multi1.result, [6,8])
         
     def test_list_by_list(self):
+        '''explain here what this test is doing'''
         multi2 = Multiplicator([3,4],[10,11])
         self.assertEqual(multi2.result, [30,44])
         
     def test_missing_one_argument_exception_thrown(self):
+        '''tell here what we are doing in this test'''
         self.assertRaises(TypeError, Multiplicator([10,11]))
 
 if __name__ == '__main__':

dc1df2e7e7acd3373504c8a8b39cdd75a66ae795
JeanBilheux <bilheuxjm@ornl.gov>
2015-08-24 15:41:41 -0400

    QSettings works

--- UserConfiguration/UsingQSettings/interface_example.py
+++ UserConfiguration/UsingQSettings/interface_example.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'interface_example.ui'
 #
-# Created: Mon Aug 24 10:21:48 2015
+# Created: Mon Aug 24 15:23:38 2015
 #      by: PyQt4 UI code generator 4.11.3
 #
 # WARNING! All changes made in this file will be lost!
@@ -26,18 +26,26 @@ except AttributeError:
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName(_fromUtf8("MainWindow"))
-        MainWindow.resize(212, 143)
+        MainWindow.resize(518, 498)
         self.centralwidget = QtGui.QWidget(MainWindow)
         self.centralwidget.setObjectName(_fromUtf8("centralwidget"))
-        self.horizontalLayout = QtGui.QHBoxLayout(self.centralwidget)
+        self.verticalLayout = QtGui.QVBoxLayout(self.centralwidget)
+        self.verticalLayout.setObjectName(_fromUtf8("verticalLayout"))
+        self.textEdit = QtGui.QTextEdit(self.centralwidget)
+        self.textEdit.setObjectName(_fromUtf8("textEdit"))
+        self.verticalLayout.addWidget(self.textEdit)
+        self.horizontalLayout = QtGui.QHBoxLayout()
         self.horizontalLayout.setObjectName(_fromUtf8("horizontalLayout"))
-        self.label = QtGui.QLabel(self.centralwidget)
-        self.label.setAutoFillBackground(False)
-        self.label.setObjectName(_fromUtf8("label"))
-        self.horizontalLayout.addWidget(self.label)
+        self.load = QtGui.QPushButton(self.centralwidget)
+        self.load.setObjectName(_fromUtf8("load"))
+        self.horizontalLayout.addWidget(self.load)
+        self.save = QtGui.QPushButton(self.centralwidget)
+        self.save.setObjectName(_fromUtf8("save"))
+        self.horizontalLayout.addWidget(self.save)
+        self.verticalLayout.addLayout(self.horizontalLayout)
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtGui.QMenuBar(MainWindow)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 212, 22))
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 518, 22))
         self.menubar.setObjectName(_fromUtf8("menubar"))
         MainWindow.setMenuBar(self.menubar)
         self.statusbar = QtGui.QStatusBar(MainWindow)
@@ -45,9 +53,12 @@ class Ui_MainWindow(object):
         MainWindow.setStatusBar(self.statusbar)
 
         self.retranslateUi(MainWindow)
+        QtCore.QObject.connect(self.load, QtCore.SIGNAL(_fromUtf8("clicked()")), MainWindow.load_settings)
+        QtCore.QObject.connect(self.save, QtCore.SIGNAL(_fromUtf8("clicked()")), MainWindow.save_settings)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow", None))
-        self.label.setText(_translate("MainWindow", "TextLabel", None))
+        self.load.setText(_translate("MainWindow", "Load", None))
+        self.save.setText(_translate("MainWindow", "Save", None))
 
--- UserConfiguration/UsingQSettings/run_application.py
+++ UserConfiguration/UsingQSettings/run_application.py
@@ -5,18 +5,25 @@ import sys
 
 class MyApplication(QtGui.QMainWindow):
     
+    _save_directory = None
+    
     def __init__(self, parent=None):
         QtGui.QMainWindow.__init__(self)
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
 
-        image = 'check_icon.png'
-        pixmap = QtGui.QPixmap(image)
-        self.ui.label.setFixedWidth(25)
-        self.ui.label.setFixedHeight(25)
-        self.ui.label.setPixmap(pixmap)
+        settings = QtCore.QSettings()
+        self._save_directory = settings.value("saved_text", '')
+
+    def save_settings(self):
+        text = self.ui.textEdit.toPlainText()
+        settings = QtCore.QSettings()
+        settings.setValue("saved_text", text)
         
-        
+    def load_settings(self):
+        settings = QtCore.QSettings()
+        self.ui.textEdit.setText(settings.value("saved_text"))
+
 if __name__ == "__main__":
     app = QtGui.QApplication(sys.argv)
     mplQt = MyApplication()

e6053666333f357f0c0fc8c96fafc44852775569
Wenduo Zhou <zhouw@ornl.gov>
2016-02-15 10:43:33 -0500

    Refs #2. Synchronized some common tools used among projects.

--- Trees/TreeViewDemo.py
+++ Trees/TreeViewDemo.py
@@ -80,8 +80,11 @@ class TreeViewWindow(QtGui.QMainWindow):
         self.connect(self.ui.pushButton_getCurrentPath, QtCore.SIGNAL('clicked()'),
                      self.do_get_current_path)
 
-        return
+        # Mouse response
+        self.connect(self.ui.treeView_custom, QtCore.SIGNAL('itemDoubleClicked()'),
+                     self.evt_double_clicked)
 
+        return
 
     def do_add_ipts(self):
         """
@@ -360,6 +363,14 @@ class TreeViewWindow(QtGui.QMainWindow):
 
         return
 
+    def evt_double_clicked(self):
+        """
+
+        :return:
+        """
+        print '[DB] Double clicked!!!'
+
+        return
 
 if __name__ == "__main__":
     app = QtGui.QApplication(sys.argv)
